475390cd988986a963872fd67f63700dc20aef98
Yann <baradhurr@gmail.com>
2019-02-23 18:38:44 +1100

    task three: adding unit test for calculator factory

---
+++ BusinessDayCounterWebApp.UnitsTests/ServicesTests/PublicHolidayCalculatorFactoryTests.cs
@@ -0,0 +1,32 @@
+﻿using BusinessDayCounterWebApp.Models;
+using BusinessDayCounterWebApp.Services;
+using BusinessDayCounterWebApp.Services.PublicHolidayCalculators;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace BusinessDayCounterWebApp.UnitTests.ServicesTests
+{
+    public class PublicHolidayCalculatorFactoryTests
+    {
+        public static IEnumerable<object[]> PublicHolidaysTypeList => new[]
+        {
+            new object[] { new PublicHoliday { HolidayType = PublicHolidayType.FixedDate }, typeof(FixedPublicHolidayCalculator) },
+        };
+
+        [Theory]
+        [MemberData(nameof(PublicHolidaysTypeList))]
+        public void GetCalculator_WhenPassedHoliday_ThenReturnsCorrectCalculator(PublicHoliday publicHoliday, Type typeOfCalculator)
+        {
+            // Arrange
+            var target = new PublicHolidayCalculatorFactory();
+
+            // Act
+            var actual = target.GetCalculator(publicHoliday);
+
+            // Assert
+            actual.GetType().Should().Be(typeOfCalculator);
+        }
+    }
+}
