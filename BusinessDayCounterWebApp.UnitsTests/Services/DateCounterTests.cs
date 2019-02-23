f146ad993b2ab15f1832b408c56faf23eebb8c9d
Yann <baradhurr@gmail.com>
2019-02-23 13:02:28 +1100

    Task One: Implementing WeekdaysBetweenTwoDates with unit tests

--- BusinessDayCounterWebApp.UnitsTests/Services/DateCounterTests.cs
+++ BusinessDayCounterWebApp.UnitsTests/Services/DateCounterTests.cs
@@ -1,13 +1,33 @@
+using BusinessDayCounterWebApp.Services;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace BusinessDayCounterWebApp.UnitsTests
 {
     public class DateCounterTests
     {
-        [Fact]
-        public void Test1()
+        public static IEnumerable<object[]> Dates => new[]
         {
+            new object[] { new DateTime(2013, 10, 7), new DateTime(2013, 10, 9), 1 },
+            new object[] { new DateTime(2013, 10, 5), new DateTime(2013, 10, 14), 5 },
+            new object[] { new DateTime(2013, 10, 7), new DateTime(2014, 1, 1), 61 },
+            new object[] { new DateTime(2013, 10, 7), new DateTime(2013, 10, 5), 0 },
+        };
 
+        [Theory]
+        [MemberData(nameof(Dates))]
+        public void WeekdaysBetweenTwoDates_WhenPasseValidDates_ThenReturnsCorrectCount(DateTime firstDate, DateTime secondDate, int expectedCounts)
+        {
+            // Arrange
+            var target = new DateCounter();
+
+            // Act
+            var actual = target.WeekdaysBetweenTwoDates(firstDate, secondDate);
+
+            // Assert
+            actual.Should().Be(expectedCounts);
         }
     }
 }
--- BusinessDayCounterWebApp/Services/DateCounter.cs
+++ BusinessDayCounterWebApp/Services/DateCounter.cs
@@ -4,7 +4,10 @@ using System.Collections.Generic;
 namespace BusinessDayCounterWebApp.Services
 {
     public class DateCounter : IDateCounter
-    {
+    {   
+        private DayOfWeek firstDayOfWeek = DayOfWeek.Monday;
+        private DayOfWeek lastDayOfWeek = DayOfWeek.Friday;
+
         public int BusinessDaysBetweenTwoDates(DateTime firstDate, DateTime secondDate, IList<DateTime> publicHolidays)
         {
             throw new NotImplementedException();
@@ -12,7 +15,42 @@ namespace BusinessDayCounterWebApp.Services
 
         public int WeekdaysBetweenTwoDates(DateTime firstDate, DateTime secondDate)
         {
-            throw new NotImplementedException();
+            var totalDaysCount = (secondDate - firstDate).Days - 1;
+
+            if (totalDaysCount <= 0)
+                return 0;
+
+            var totalWeeksCount = totalDaysCount / 7;
+            var weekDaysInFullWeek = totalWeeksCount * 5;
+
+            var remainingDays = CalculateRemainingDays(firstDate, totalDaysCount);
+
+            return weekDaysInFullWeek + remainingDays;
+        }
+
+        private int CalculateRemainingDays(DateTime firstDate, int totalDaysCount)
+        {
+            var remaingDays = totalDaysCount % 7;
+            var daysToWeekend = DayOfWeek.Saturday - firstDate.DayOfWeek;
+                       
+            // Remaining days are within a week, before the weekend
+            if (remaingDays <= daysToWeekend)
+            {
+                return remaingDays;
+            }
+
+            // Remaining days are within a week, but ends on weekend
+            // Or stepping over the weekend
+            if (remaingDays <= daysToWeekend + 2)
+            {
+                remaingDays = daysToWeekend;
+            }
+            else
+            {
+                remaingDays -= 2;
+            }
+
+            return remaingDays;
         }
     }
 }
